b7e197a36c8445d735105d5ca849c5a9e3ab2634
kodowaniepo30 <kodowaniepo30@gmail.com>
2016-08-18 16:07:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LiczbyPierwsze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+            bool itIs = false;
+            for (int j = 1; j <= n; j++)
+            {           
+                itIs = true;
+                for (int i = 2; i * i <= j; i++)
+                {
+                    if (j % i == 0)
+                        itIs = false;
+                }
+                Console.Write(j);
+                if (j == 1)
+                    itIs = false;
+                if (itIs)
+                    Console.WriteLine(" TAK");
+                else
+                    Console.WriteLine(" NIE");                 
+            }
+            Console.ReadLine();
+        }
+    }
+}
